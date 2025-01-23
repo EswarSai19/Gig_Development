10c51f41d0642ec7e9c3c83ddd88ee058fa67ac4
EswarSai19 <eswarsai219@gmail.com>
2025-01-23 18:43:30 +0530

    Finally al the working links has been attached and everthing is working fine in NR flow

--- Dev_Dev/gigzera/non_register/static/js/navbar.js
+++ Dev_Dev/gigzera/non_register/static/js/navbar.js
@@ -1,7 +1,7 @@
 // pop up js code for contact us
 document.addEventListener("DOMContentLoaded", function () {
   const contactBtn = document.getElementById("contactBtn");
-  //   const contactBtn2 = document.getElementById("contactBtn2");
+  // const contactBtn3 = document.getElementById("contactBtn3");
   const popupPanel = document.getElementById("popupPanel");
   const popupOverlay = document.getElementById("popupOverlay");
   const closeBtn = document.getElementById("closeBtn");
@@ -11,10 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
     popupOverlay.style.display = "block"; // Show overlay
     popupPanel.style.right = "0"; // Slide in sidebar
   };
-  //   contactBtn2.onclick = function () {
-  //     popupOverlay.style.display = "block"; // Show overlay
-  //     popupPanel.style.right = "0"; // Slide in sidebar
-  //   };
+  // contactBtn3.onclick = function () {
+  //   popupOverlay.style.display = "block"; // Show overlay
+  //   popupPanel.style.right = "0"; // Slide in sidebar
+  // };
   // Function to close sidebar
   function closeSidebar() {
     popupOverlay.style.display = "none"; // Hide overlay
